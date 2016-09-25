ef45264fe508e43be3b604402b2dd04b32e77cb8
Mervin Felix <mrvnflx@gmail.com>
2016-09-24 23:36:34 -0400

    Add support for spring cloud config client to dynamically refresh properties from a config server

--- src/main/java/com/mrvnflx/spring/mvc/controller/SpringMvcController.java
+++ src/main/java/com/mrvnflx/spring/mvc/controller/SpringMvcController.java
@@ -1,18 +1,23 @@
 package com.mrvnflx.spring.mvc.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by mrvnf on 9/20/2016.
  */
-@Controller
+@RefreshScope
+@RestController
 public class SpringMvcController {
 
+    @Value("${spring.mvc.hello.message:Hello, SpringMvcController!}")
+    private String helloMessage;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    @ResponseBody public String hello() {
-        return "Hello, SpringMvcController!";
+    public String hello() {
+        return helloMessage;
     }
 }
